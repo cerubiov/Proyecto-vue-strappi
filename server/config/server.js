8621c2c284e6cb9c3a8386874c3b7eaf0031ea20
cerubiov <92275676+cerubiov@users.noreply.github.com>
2021-10-10 16:33:37 -0500

    Add files via upload

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8869574be0b48130c001e76d47ca544a'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8b69446d-938a-4ea7-b360-c2b22fec4325'
+};
